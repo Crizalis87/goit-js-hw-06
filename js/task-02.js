ab055b26baa8ab5f1112c38616341caccba9804b
Crizalis87 <dmytroyermolayev@gmail.com>
2022-06-15 01:06:05 +0300

    update task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  const listItem = document.createElement("li");
+  listItem.textContent = ingredients[i];
+  console.log(listItem.textContent);
+  list.append(listItem);
+}
