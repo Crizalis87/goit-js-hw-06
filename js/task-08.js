54b9a36edca1e348109e107f1bae138aed90cf1c
Crizalis87 <dmytroyermolayev@gmail.com>
2022-06-16 01:40:39 +0300

    upate task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const formSignIn = document.querySelector(".login-form");
 
+formSignIn.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("All fields must be filled!");
+  }
+
+  const dataSubmit = { email: email.value, password: password.value };
+  console.log(dataSubmit);
+  event.currentTarget.reset();
+});
