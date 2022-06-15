cad80b82d79362046ef27ca5c2f552ca5396982f
Crizalis87 <dmytroyermolayev@gmail.com>
2022-06-15 18:57:51 +0300

    update task5

---
+++ js/task-05.js
@@ -0,0 +1,15 @@
+const inputName = document.querySelector("#name-input");
+const outputName = document.querySelector("#name-output");
+
+inputName.addEventListener("input", (event) => {
+  outputName.textContent = event.currentTarget.value;
+
+  if (event.currentTarget.value === "") {
+    outputName.textContent = "Anonymous";
+  }
+});
+
+// inputName.addEventListener("blur", () => {
+//   inputName.value = "";
+//   outputName.textContent = "Anonymous";
+// });
